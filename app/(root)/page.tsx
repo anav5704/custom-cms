dae52b16d0ee400fbe74f6a4d3cfaf3bd95f3065
anav5704 <anavkki@gmail.com>
2023-11-11 11:11:04 +1200

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+        <h1>Custom CMS</h1>
+        <Button>Hello World</Button>
+    </>
+  )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Custom CMS',
+  description: 'This is a custom build content management system made to manage the ecommerse website I had made.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
